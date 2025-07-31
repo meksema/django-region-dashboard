2946fa5d9a5aeeed40dc677d4b79053fbc602d66
meksema <musemamekiya847@gmail.com>
2025-07-31 14:19:12 +0300

    Update urls.py

--- let/urls.py
+++ let/urls.py
@@ -1,9 +1,6 @@
-# let/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
