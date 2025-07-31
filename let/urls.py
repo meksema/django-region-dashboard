ca7038f1d981c535813ae5fe61b24b1d4aea78ae
meksema <musemamekiya847@gmail.com>
2025-07-31 14:17:44 +0300

    Update urls.py

--- let/urls.py
+++ let/urls.py
@@ -8,15 +8,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    
-    path('login/',  auth_views.LoginView.as_view(template_name='login.html'),  name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'),name='logout'),
-    path('', include('myapp.urls')), 
-
     
     path('api/', include('myapp.api_urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
